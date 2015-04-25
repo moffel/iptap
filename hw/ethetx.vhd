eea385c29c4344cfdae4b6c05fee541e8909ade3
Michael Offel <michael.offel@noumenastudios.com>
2015-04-25 22:12:00 +0200

    at ethernet tx unit (working)

---
+++ hw/ethetx.vhd
@@ -0,0 +1,190 @@
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx primitives in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+entity ethetx is
+	Port (	clk, rst : in std_logic;
+				
+				frame_addr : in std_logic_vector(10 downto 0);
+				frame_data : in std_logic_vector(7 downto 0);
+				frame_write_enable : in std_logic;
+				
+				send_frame : in std_logic;
+				frame_sent : out std_logic;
+				
+				tx_clk : in std_logic;
+				tx_dv : out std_logic;
+				tx_d : out std_logic_vector(3 downto 0)
+			);
+end ethetx;
+
+architecture Behavioral of ethetx is
+
+   COMPONENT trigger
+   PORT( IN_D	:	IN	STD_LOGIC; 
+          IN_CLK	:	IN	STD_LOGIC; 
+          OUT_CLK	:	IN	STD_LOGIC; 
+          OUT_D	:	OUT	STD_LOGIC);
+   END COMPONENT;
+
+	COMPONENT crc
+	PORT(
+		data_in : IN std_logic_vector(7 downto 0);
+		crc_en : IN std_logic;
+		rst : IN std_logic;
+		clk : IN std_logic;          
+		crc_out : OUT std_logic_vector(31 downto 0)
+		);
+	END COMPONENT;
+
+	type state_type is (IDLE, PREAMBLE, DATA, CHECKSUM, WAITING);
+	signal state, next_state: state_type;
+
+	signal send_frame_trigger : std_logic;
+	signal frame_sent_trigger : std_logic;
+	
+	signal counter : std_logic_vector(11 downto 0); -- counter counts nibbles not bytes
+	
+	type frame_memory_type is array(0 to 1535) of std_logic_vector(7 downto 0);
+	signal frame_memory : frame_memory_type;
+	signal tx_byte : std_logic_vector(7 downto 0);
+	signal tx_byte_rev : std_logic_vector(7 downto 0);
+	signal tx_crc : std_logic_vector(31 downto 0);
+	signal tx_crc_rev : std_logic_vector(31 downto 0);
+	signal tx_crc_rst : std_logic;
+	signal tx_crc_en : std_logic;
+begin
+
+-- memory signals
+
+	process(clk)
+	begin
+		if clk'event and clk = '1' then
+			if frame_write_enable = '1' then
+				frame_memory(to_integer(unsigned(frame_addr))) <= frame_data;
+			end if;
+		end if;
+	end process;
+
+	tx_byte <= 	"11010101" when state = PREAMBLE and next_state = DATA else
+					"01010101" when state = PREAMBLE else
+					not tx_crc_rev( 7 downto  0) when state = CHECKSUM and counter(2 downto 1) = "00" else
+					not tx_crc_rev(15 downto  8) when state = CHECKSUM and counter(2 downto 1) = "01" else
+					not tx_crc_rev(23 downto 16) when state = CHECKSUM and counter(2 downto 1) = "10" else
+					not tx_crc_rev(31 downto 24) when state = CHECKSUM and counter(2 downto 1) = "11" else
+					frame_memory(to_integer(unsigned(counter(11 downto 1))));
+
+	tx_d <=	tx_byte(3 downto 0) when counter(0) = '0' else
+				tx_byte(7 downto 4);
+
+	tx_dv <= '1' when state = PREAMBLE or state = DATA or state = CHECKSUM else '0';
+
+-- crc
+
+	Inst_crc: crc PORT MAP(
+		data_in => tx_byte_rev,
+		crc_en => tx_crc_en,
+		rst => tx_crc_rst,
+		clk => tx_clk,
+		crc_out => tx_crc
+	);
+	
+	tx_byte_reverser : 
+	for I in 0 to 7 generate
+--		tx_byte_rev(7-I) <= not tx_byte(I) when unsigned(counter) < 8 else tx_byte(I);
+		tx_byte_rev(7-I) <= tx_byte(I);
+	end generate;
+	
+	tx_crc_reverser:
+	for I in 0 to 31 generate
+		tx_crc_rev(31-I) <= tx_crc(I);
+	end generate;
+
+	tx_crc_en <= '1' when state = DATA and counter(0) = '0' else '0';
+	tx_crc_rst <= '1' when state = PREAMBLE else '0';
+
+-- fsm
+
+	process(tx_clk, rst)
+	begin
+	
+		if rst = '1' then
+			counter <= (others => '0');
+			state <= IDLE;
+		elsif tx_clk'event and tx_clk = '1' then
+			state <= next_state;
+			
+			if state = next_state then
+				counter <= std_logic_vector(unsigned(counter) + 1);
+			else
+				counter <= (others => '0');
+			end if;
+					
+		end if;
+	
+	end process;
+
+
+	process(state, send_frame_trigger, counter)
+	begin
+		next_state <= state;
+	
+		case state is
+		
+			when IDLE =>
+				if send_frame_trigger = '1' then
+					next_state <= PREAMBLE;
+				end if;
+		
+			when PREAMBLE =>
+				if counter(3 downto 0) = "1111" then
+					next_state <= DATA;
+				end if;
+				
+			when DATA =>
+				if counter = frame_addr & '1' then
+					next_state <= CHECKSUM;
+				end if;
+				
+			when CHECKSUM =>
+				if counter(2 downto 0) = "111" then
+					next_state <= WAITING;
+				end if;
+				
+			when WAITING =>
+				if counter(4) = '1' then
+					next_state <= IDLE;
+				end if;
+				
+		end case;
+	end process;
+
+
+-- crossing clock domains
+
+	out_trigger: trigger
+	port map (
+		IN_CLK => tx_clk,
+		IN_D => frame_sent_trigger,
+		OUT_CLK => clk,
+		OUT_D => frame_sent
+	);
+	
+	in_trigger: trigger
+	port map (
+		IN_CLK => clk,
+		IN_D => send_frame,
+		OUT_CLK => tx_clk,
+		OUT_D => send_frame_trigger
+	);
+
+end Behavioral;
+
---
+++ hw/test_ethetx.vhd
@@ -0,0 +1,323 @@
+--------------------------------------------------------------------------------
+-- Company: 
+-- Engineer:
+--
+-- Create Date:   19:08:05 04/25/2015
+-- Design Name:   
+-- Module Name:   D:/daten/source/ISE/DS/iptap/hw/test_ethetx.vhd
+-- Project Name:  iptap
+-- Target Device:  
+-- Tool versions:  
+-- Description:   
+-- 
+-- VHDL Test Bench Created by ISE for module: ethetx
+-- 
+-- Dependencies:
+-- 
+-- Revision:
+-- Revision 0.01 - File Created
+-- Additional Comments:
+--
+-- Notes: 
+-- This testbench has been automatically generated using types std_logic and
+-- std_logic_vector for the ports of the unit under test.  Xilinx recommends
+-- that these types always be used for the top-level I/O of a design in order
+-- to guarantee that the testbench will bind correctly to the post-implementation 
+-- simulation model.
+--------------------------------------------------------------------------------
+LIBRARY ieee;
+USE ieee.std_logic_1164.ALL;
+ 
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+USE ieee.numeric_std.ALL;
+ 
+ENTITY test_ethetx IS
+END test_ethetx;
+ 
+ARCHITECTURE behavior OF test_ethetx IS 
+ 
+    -- Component Declaration for the Unit Under Test (UUT)
+ 
+    COMPONENT ethetx
+    PORT(
+         clk : IN  std_logic;
+         rst : IN  std_logic;
+         frame_addr : IN  std_logic_vector(10 downto 0);
+         frame_data : IN  std_logic_vector(7 downto 0);
+         frame_write_enable : IN  std_logic;
+         send_frame : IN  std_logic;
+         frame_sent : OUT  std_logic;
+         tx_clk : IN  std_logic;
+         tx_dv : OUT  std_logic;
+         tx_d : OUT  std_logic_vector(3 downto 0)
+        );
+    END COMPONENT;
+    
+	COMPONENT etherx
+	PORT(
+		rst : IN std_logic;
+		clk : IN std_logic;
+		rx_clk : IN std_logic;
+		rx_d : IN std_logic_vector(3 downto 0);
+		rx_dv : IN std_logic;
+		o_addr : IN std_logic_vector(10 downto 0);
+		o_done : IN std_logic;          
+		o_ready : OUT std_logic;
+		o_data : OUT std_logic_vector(7 downto 0)
+		);
+	END COMPONENT;
+
+   --Inputs
+   signal clk : std_logic := '0';
+   signal rst : std_logic := '0';
+   signal frame_addr : std_logic_vector(10 downto 0) := (others => '0');
+   signal frame_data : std_logic_vector(7 downto 0) := (others => '0');
+   signal frame_write_enable : std_logic := '0';
+   signal send_frame : std_logic := '0';
+   signal tx_clk : std_logic := '0';
+
+ 	--Outputs
+   signal frame_sent : std_logic;
+   signal tx_dv : std_logic;
+   signal tx_d : std_logic_vector(3 downto 0);
+
+   -- Clock period definitions
+   constant clk_period : time := 6 ns;
+   constant tx_clk_period : time := 20 ns;
+ 
+
+	signal rx_ready, rx_done : std_logic;
+	signal rx_data : std_logic_vector(7 downto 0);
+BEGIN
+ 
+	-- Instantiate the Unit Under Test (UUT)
+   uut: ethetx PORT MAP (
+          clk => clk,
+          rst => rst,
+          frame_addr => frame_addr,
+          frame_data => frame_data,
+          frame_write_enable => frame_write_enable,
+          send_frame => send_frame,
+          frame_sent => frame_sent,
+          tx_clk => tx_clk,
+          tx_dv => tx_dv,
+          tx_d => tx_d
+        );
+
+	Inst_etherx: etherx PORT MAP(
+		rst => rst,
+		clk => clk,
+		rx_clk => tx_clk,
+		rx_d => tx_d,
+		rx_dv => tx_dv,
+		o_ready => rx_ready,
+		o_addr => (others => '0'),
+		o_data => rx_data,
+		o_done => rx_done
+	);
+
+   -- Clock process definitions
+   clk_process :process
+   begin
+		clk <= '0';
+		wait for clk_period/2;
+		clk <= '1';
+		wait for clk_period/2;
+   end process;
+ 
+   tx_clk_process :process
+   begin
+		tx_clk <= '0';
+		wait for tx_clk_period/2;
+		tx_clk <= '1';
+		wait for tx_clk_period/2;
+   end process;
+ 
+
+   -- Stimulus process
+   stim_proc: process
+   begin		
+      -- hold reset state for 100 ns.
+		rst <= '1';
+      wait for 100 ns;	
+		rst <= '0';
+
+		-- setup memory
+		frame_write_enable <= '1';
+		
+		frame_addr <= (others => '0');
+		frame_data <= x"ff";
+		wait for clk_period;
+		frame_addr <= std_logic_vector(unsigned(frame_addr) + 1);
+		frame_data <= x"ff";
+		wait for clk_period;
+		frame_addr <= std_logic_vector(unsigned(frame_addr) + 1);
+		frame_data <= x"ff";
+		wait for clk_period;
+		frame_addr <= std_logic_vector(unsigned(frame_addr) + 1);
+		frame_data <= x"ff";
+		wait for clk_period;
+		frame_addr <= std_logic_vector(unsigned(frame_addr) + 1);
+		frame_data <= x"ff";
+		wait for clk_period;
+		frame_addr <= std_logic_vector(unsigned(frame_addr) + 1);
+		frame_data <= x"ff";
+		wait for clk_period;
+		frame_addr <= std_logic_vector(unsigned(frame_addr) + 1);
+		frame_data <= x"90";
+		wait for clk_period;
+		frame_addr <= std_logic_vector(unsigned(frame_addr) + 1);
+		frame_data <= x"e6";
+		wait for clk_period;
+		frame_addr <= std_logic_vector(unsigned(frame_addr) + 1);
+		frame_data <= x"ba";
+		wait for clk_period;
+		frame_addr <= std_logic_vector(unsigned(frame_addr) + 1);
+		frame_data <= x"43";
+		wait for clk_period;
+		frame_addr <= std_logic_vector(unsigned(frame_addr) + 1);
+		frame_data <= x"1d";
+		wait for clk_period;
+		frame_addr <= std_logic_vector(unsigned(frame_addr) + 1);
+		frame_data <= x"b0";
+		wait for clk_period;
+		frame_addr <= std_logic_vector(unsigned(frame_addr) + 1);
+		frame_data <= x"08";
+		wait for clk_period;
+		frame_addr <= std_logic_vector(unsigned(frame_addr) + 1);
+		frame_data <= x"06";
+		wait for clk_period;
+		frame_addr <= std_logic_vector(unsigned(frame_addr) + 1);
+		frame_data <= x"00";
+		wait for clk_period;
+		frame_addr <= std_logic_vector(unsigned(frame_addr) + 1);
+		frame_data <= x"01";
+		wait for clk_period;
+		frame_addr <= std_logic_vector(unsigned(frame_addr) + 1);
+		frame_data <= x"08";
+		wait for clk_period;
+		frame_addr <= std_logic_vector(unsigned(frame_addr) + 1);
+		frame_data <= x"00";
+		wait for clk_period;
+		frame_addr <= std_logic_vector(unsigned(frame_addr) + 1);
+		frame_data <= x"06";
+		wait for clk_period;
+		frame_addr <= std_logic_vector(unsigned(frame_addr) + 1);
+		frame_data <= x"04";
+		wait for clk_period;
+		frame_addr <= std_logic_vector(unsigned(frame_addr) + 1);
+		frame_data <= x"00";
+		wait for clk_period;
+		frame_addr <= std_logic_vector(unsigned(frame_addr) + 1);
+		frame_data <= x"01";
+		wait for clk_period;
+		frame_addr <= std_logic_vector(unsigned(frame_addr) + 1);
+		frame_data <= x"90";
+		wait for clk_period;
+		frame_addr <= std_logic_vector(unsigned(frame_addr) + 1);
+		frame_data <= x"e6";
+		wait for clk_period;
+		frame_addr <= std_logic_vector(unsigned(frame_addr) + 1);
+		frame_data <= x"ba";
+		wait for clk_period;
+		frame_addr <= std_logic_vector(unsigned(frame_addr) + 1);
+		frame_data <= x"43";
+		wait for clk_period;
+		frame_addr <= std_logic_vector(unsigned(frame_addr) + 1);
+		frame_data <= x"1d";
+		wait for clk_period;
+		frame_addr <= std_logic_vector(unsigned(frame_addr) + 1);
+		frame_data <= x"b0";
+		wait for clk_period;
+		frame_addr <= std_logic_vector(unsigned(frame_addr) + 1);
+		frame_data <= x"0a";
+		wait for clk_period;
+		frame_addr <= std_logic_vector(unsigned(frame_addr) + 1);
+		frame_data <= x"00";
+		wait for clk_period;
+		frame_addr <= std_logic_vector(unsigned(frame_addr) + 1);
+		frame_data <= x"00";
+		wait for clk_period;
+		frame_addr <= std_logic_vector(unsigned(frame_addr) + 1);
+		frame_data <= x"01";
+		wait for clk_period;
+		frame_addr <= std_logic_vector(unsigned(frame_addr) + 1);
+		frame_data <= x"00";
+		wait for clk_period;
+		frame_addr <= std_logic_vector(unsigned(frame_addr) + 1);
+		frame_data <= x"00";
+		wait for clk_period;
+		frame_addr <= std_logic_vector(unsigned(frame_addr) + 1);
+		frame_data <= x"00";
+		wait for clk_period;
+		frame_addr <= std_logic_vector(unsigned(frame_addr) + 1);
+		frame_data <= x"00";
+		wait for clk_period;
+		frame_addr <= std_logic_vector(unsigned(frame_addr) + 1);
+		frame_data <= x"00";
+		wait for clk_period;
+		frame_addr <= std_logic_vector(unsigned(frame_addr) + 1);
+		frame_data <= x"00";
+		wait for clk_period;
+		frame_addr <= std_logic_vector(unsigned(frame_addr) + 1);
+		frame_data <= x"0a";
+		wait for clk_period;
+		frame_addr <= std_logic_vector(unsigned(frame_addr) + 1);
+		frame_data <= x"00";
+		wait for clk_period;
+		frame_addr <= std_logic_vector(unsigned(frame_addr) + 1);
+		frame_data <= x"00";
+		wait for clk_period;
+		frame_addr <= std_logic_vector(unsigned(frame_addr) + 1);
+		frame_data <= x"04";
+		wait for clk_period;
+		
+		frame_data <= x"00";
+		frame_addr <= std_logic_vector(unsigned(frame_addr) + 1);
+		wait for clk_period;
+		frame_addr <= std_logic_vector(unsigned(frame_addr) + 1);
+		wait for clk_period;
+		frame_addr <= std_logic_vector(unsigned(frame_addr) + 1);
+		wait for clk_period;
+		frame_addr <= std_logic_vector(unsigned(frame_addr) + 1);
+		wait for clk_period;
+		frame_addr <= std_logic_vector(unsigned(frame_addr) + 1);
+		wait for clk_period;
+		frame_addr <= std_logic_vector(unsigned(frame_addr) + 1);
+		wait for clk_period;
+		frame_addr <= std_logic_vector(unsigned(frame_addr) + 1);
+		wait for clk_period;
+		frame_addr <= std_logic_vector(unsigned(frame_addr) + 1);
+		wait for clk_period;
+		frame_addr <= std_logic_vector(unsigned(frame_addr) + 1);
+		wait for clk_period;
+		frame_addr <= std_logic_vector(unsigned(frame_addr) + 1);
+		wait for clk_period;
+		frame_addr <= std_logic_vector(unsigned(frame_addr) + 1);
+		wait for clk_period;
+		frame_addr <= std_logic_vector(unsigned(frame_addr) + 1);
+		wait for clk_period;
+		frame_addr <= std_logic_vector(unsigned(frame_addr) + 1);
+		wait for clk_period;
+		frame_addr <= std_logic_vector(unsigned(frame_addr) + 1);
+		wait for clk_period;
+		frame_addr <= std_logic_vector(unsigned(frame_addr) + 1);
+		wait for clk_period;
+		frame_addr <= std_logic_vector(unsigned(frame_addr) + 1);
+		wait for clk_period;
+		frame_addr <= std_logic_vector(unsigned(frame_addr) + 1);
+		wait for clk_period;
+		frame_addr <= std_logic_vector(unsigned(frame_addr) + 1);
+		wait for clk_period;
+		frame_write_enable <= '0';
+		
+		send_frame <= '1';
+		wait for clk_period;
+		send_frame <= '0';
+      -- insert stimulus here 
+
+      wait;
+   end process;
+
+END;
